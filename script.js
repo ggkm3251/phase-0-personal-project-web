48e29b1ec7654012bce5bc161364b5e1b765b39a
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-07 01:13:36 +0300

    make changes to script.js

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ function testResults (form) {
     var TestVar = form.inputbox.value;
     alert ("You typed: " + TestVar);
 }
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'CODE G!';
